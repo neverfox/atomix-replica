0dc6d05b9b08c66d156ee872b98422c45e7f4f50
Roman Pearah <github.to.soma@neverbox.com>
2016-07-05 11:49:42 -0400

    Upgrade dependencies and add logback

--- project.clj
+++ project.clj
@@ -3,13 +3,16 @@
   :url "http://example.com/FIXME"
   :dependencies [[beckon "0.1.1"]
                  [environ "1.0.3"]
+                 [ch.qos.logback/logback-classic "1.1.7"]
+                 [ch.qos.logback/logback-core "1.1.7"]
                  [com.fzakaria/slf4j-timbre "0.3.2"]
                  [com.taoensso/timbre "4.3.1"]
                  [im.chit/hara.component "2.3.6"]
-                 [io.atomix/atomix-all "1.0.0-rc7"]
+                 [io.atomix/atomix-all "1.0.0-rc9"]
                  [kubernetes-api "0.1.0"]
                  [org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.374"]]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.slf4j/slf4j-api "1.7.21"]]
   :main ^:skip-aot atomix-playground.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
