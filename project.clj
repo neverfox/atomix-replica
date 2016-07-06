2fb8219733aaa6383dd0527bb6903a271fefa883
Roman Pearah <github.to.soma@neverbox.com>
2016-07-06 03:05:16 -0400

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject atomix-replica "0.2.0-SNAPSHOT"
+(defproject atomix-replica "0.2.0"
   :description "Atomix replica as a Clojure system"
   :url "https://github.com/neverfox/atomix-replica"
   :license {:name "Eclipse Public License"
